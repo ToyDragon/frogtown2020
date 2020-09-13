62fedfd72a495e92f9a80d786dba04564302574a
Frogtown Matt <batesmatthewj@gmail.com>
2020-09-12 21:03:59 -0500

    use lint styling

--- src/server/setupchecker/setupchecker.ts
+++ src/server/setupchecker/setupchecker.ts
@@ -4,13 +4,11 @@ import Services from "../services";
 import CheckForStorageIssue from "./checker_storage";
 import CheckForDBIssue from "./checker_db";
 
-/**
- * Primary entry point for checking issues during server startup.
- * This acts as a proxy to all other setup checking function.
- * @param {Config} config
- */
-// eslint-disable-next-line prettier/prettier
-export default async function CheckSetup(services: Services): Promise<SetupIssue[]> {
+// Primary entry point for checking issues during server startup.
+// This acts as a proxy to all other setup checking function.
+export default async function CheckSetup(
+  services: Services
+): Promise<SetupIssue[]> {
   let issues: SetupIssue[] = [];
   issues = issues.concat(await CheckForStorageIssue(services));
   issues = issues.concat(await CheckForDBIssue(services));
