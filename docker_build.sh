236aa5e1597d38f4de1e5bc80334906455f93d15
Frogtown Matt <batesmatthewj@gmail.com>
2021-04-24 10:28:44 -0500

    add different configs for different deployments, and build script

---
+++ docker_build.sh
@@ -0,0 +1,12 @@
+echo "Building beta version..."
+cp "config.beta.json" "config.json"
+sudo docker build -t gcr.io/frogtown/frogtown2020:beta .
+sudo docker push gcr.io/frogtown/frogtown2020:beta
+
+echo "Building prod version..."
+cp "config.prod.json" "config.json"
+sudo docker build -t gcr.io/frogtown/frogtown2020:prod .
+sudo docker push gcr.io/frogtown/frogtown2020:prod
+
+cp "config.local.json" "config.json"
+echo "Done building and restored local config."
