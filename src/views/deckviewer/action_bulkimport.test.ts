b306067fd51330da88911a8fed086b13632a5adf
Frogtown Matt <batesmatthewj@gmail.com>
2021-06-18 22:26:24 -0500

    fix some tests. Add wasteland test that fails.

--- src/views/deckviewer/action_bulkimport.test.ts
+++ src/views/deckviewer/action_bulkimport.test.ts
@@ -4,6 +4,8 @@ const idToName: Record<string, string[]> = {};
 idToName["a"] = ["1", "2"];
 idToName["b"] = ["3"];
 idToName["c"] = ["4"];
+idToName["wasteland"] = ["5"];
+idToName["waste land"] = ["6"];
 
 test("Handles simple single line.", () => {
   let result = getCardsByName("2 a", idToName);
@@ -42,20 +44,28 @@ test("Handles multiple lines.", () => {
 test("Handles whitespace gracefully", () => {
   const result = getCardsByName("\t2 a\n\t\tb", idToName);
   expect(result.errors.length).toBe(0);
-  expect(result.ids.length).toBe(3);
+  expect(result.ids.length).toBe(2);
   expect(result.ids[0]).toBe("1");
-  expect(result.ids[1]).toBe("1");
-  expect(result.ids[2]).toBe("3");
+  expect(result.ids[1]).toBe("3");
 });
 
 test("Detects errors", () => {
-  const result = getCardsByName("\t2 a\n\t\tb\nd\ne\nb", idToName);
+  const result = getCardsByName("\t2 a\n\t\tb\nx\nz\nb\nc", idToName);
   expect(result.errors.length).toBe(2);
-  expect(result.errors[0]).toBe("d");
-  expect(result.errors[1]).toBe("e");
+  expect(result.errors[0]).toBe("x");
+  expect(result.errors[1]).toBe("z");
   expect(result.ids.length).toBe(4);
   expect(result.ids[0]).toBe("1");
-  expect(result.ids[1]).toBe("1");
+  expect(result.ids[1]).toBe("3");
   expect(result.ids[2]).toBe("3");
-  expect(result.ids[3]).toBe("3");
+  expect(result.ids[3]).toBe("4");
+});
+
+test("Wasteland Waste Land", () => {
+  // Currently fails :(
+  const result = getCardsByName("1 wasteland\n2 waste land", idToName);
+  expect(result.errors.length).toBe(0);
+  expect(result.ids.length).toBe(2);
+  expect(result.ids[0]).toBe("5");
+  expect(result.ids[1]).toBe("6");
 });
