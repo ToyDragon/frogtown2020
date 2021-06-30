39ef44e2254630ccc172a085de2f88f43a0c0c17
Frogtown Matt <batesmatthewj@gmail.com>
2021-06-30 18:18:09 -0500

    Remove sudo from docker commands. This will be ran out-of-terminal as Jenkins, so sudo is not helpful.

--- docker_build.sh
+++ docker_build.sh
@@ -6,16 +6,16 @@ fi
 
 echo "Building beta version..."
 cp "config.beta.json" "config.json"
-sudo docker build -t gcr.io/frogtown/frogtown2020/beta:$1 .
-sudo docker push gcr.io/frogtown/frogtown2020/beta:$1
+docker build -t gcr.io/frogtown/frogtown2020/beta:$1 .
+docker push gcr.io/frogtown/frogtown2020/beta:$1
 
 echo "Building prod version..."
 cp "config.prod.json" "config.json"
-sudo docker build -t gcr.io/frogtown/frogtown2020/prod:$1 .
-sudo docker push gcr.io/frogtown/frogtown2020/prod:$1
+docker build -t gcr.io/frogtown/frogtown2020/prod:$1 .
+docker push gcr.io/frogtown/frogtown2020/prod:$1
 
 echo "Building local version..."
 cp "config.local.json" "config.json"
-sudo docker build -t gcr.io/frogtown/frogtown2020/local:$1 .
+docker build -t gcr.io/frogtown/frogtown2020/local:$1 .
 
 echo "Done building and restored local config."
