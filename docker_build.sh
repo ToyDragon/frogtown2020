7487ec275e1fbf4c721e15991cf1616e1d5e44d4
Frogtown Matt <batesmatthewj@gmail.com>
2021-06-30 18:33:45 -0500

    Fallback to config files in the home directory, for Jenkins.

--- docker_build.sh
+++ docker_build.sh
@@ -4,17 +4,17 @@ if [[ -z "$1" ]] ; then
 fi
 
 echo "Building beta version..."
-cp "config.beta.json" "config.json"
+cp "config.beta.json" "config.json" || cp "~/frogtown/config/config.beta.json" "config.json";
 docker build -t gcr.io/frogtown/frogtown2020/beta:$1 .
 docker push gcr.io/frogtown/frogtown2020/beta:$1
 
 echo "Building prod version..."
-cp "config.prod.json" "config.json"
+cp "config.prod.json" "config.json" || cp "~/frogtown/config/config.prod.json" "config.json";
 docker build -t gcr.io/frogtown/frogtown2020/prod:$1 .
 docker push gcr.io/frogtown/frogtown2020/prod:$1
 
 echo "Building local version..."
-cp "config.local.json" "config.json"
+cp "config.local.json" "config.json" || cp "~/frogtown/config/config.local.json" "config.json";
 docker build -t gcr.io/frogtown/frogtown2020/local:$1 .
 
 echo "Done building and restored local config."
