0c632151a53473ce9772bf63b73841cf753fa0ab
Frogtown Matt <batesmatthewj@gmail.com>
2021-07-01 17:24:20 -0500

    add parameters for tag, and for choosing which images to build

--- docker_build.sh
+++ docker_build.sh
@@ -1,12 +1,30 @@
-echo "Building beta version..."
-cp "config.beta.json" "config.json"
-sudo docker build -t gcr.io/frogtown/frogtown2020/beta .
-sudo docker push gcr.io/frogtown/frogtown2020/beta
+if [ -z "$1" ]
+then
+  echo "Missing argument"
+  exit 1
+fi
 
-echo "Building prod version..."
-cp "config.prod.json" "config.json"
-sudo docker build -t gcr.io/frogtown/frogtown2020/prod .
-sudo docker push gcr.io/frogtown/frogtown2020/prod
+echo "Copying in secrets..."
+cp -r "/home/jenkins/frogtown/secrets" .;
 
-cp "config.local.json" "config.json"
+if [ "$2" != "local" ]
+then
+  echo "Building beta version..."
+  cp "config.beta.json" "config.json" || cp "/home/jenkins/frogtown/config/config.beta.json" "config.json";
+  docker build -t gcr.io/frogtown/frogtown2020/beta:$1 .
+  docker push gcr.io/frogtown/frogtown2020/beta:$1
+
+  echo "Building prod version..."
+  cp "config.prod.json" "config.json" || cp "/home/jenkins/frogtown/config/config.prod.json" "config.json";
+  docker build -t gcr.io/frogtown/frogtown2020/prod:$1 .
+  docker push gcr.io/frogtown/frogtown2020/prod:$1
+fi
+
+if [ "$2" != "betaprod" ]
+then
+  echo "Building local version..."
+  cp "config.local.json" "config.json" || cp "/home/jenkins/frogtown/config/config.local.json" "config.json";
+  docker build -t gcr.io/frogtown/frogtown2020/local:$1 .
+fi
+
 echo "Done building and restored local config."
