3affe470df195c6ab84be6bb7c8110577c8e878a
Frogtown Matt <batesmatthewj@gmail.com>
2021-06-30 20:36:43 -0500

    Add flag for only building local

--- Jenkinsfile
+++ Jenkinsfile
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                sh 'npm install && npm run-script build && ./docker_build.sh ${BUILD_TAG}'
+                sh 'npm install && npm run-script build && ./docker_build.sh ${BUILD_TAG} local'
             }
         }
         stage('Test') {
--- docker_build.sh
+++ docker_build.sh
@@ -7,15 +7,18 @@ fi
 echo "Copying in secrets..."
 cp -r "/home/jenkins/frogtown/secrets" .;
 
-echo "Building beta version..."
-cp "config.beta.json" "config.json" || cp "/home/jenkins/frogtown/config/config.beta.json" "config.json";
-docker build -t gcr.io/frogtown/frogtown2020/beta:$1 .
-docker push gcr.io/frogtown/frogtown2020/beta:$1
+if [ "$2" != "local" ]
+then
+  echo "Building beta version..."
+  cp "config.beta.json" "config.json" || cp "/home/jenkins/frogtown/config/config.beta.json" "config.json";
+  docker build -t gcr.io/frogtown/frogtown2020/beta:$1 .
+  docker push gcr.io/frogtown/frogtown2020/beta:$1
 
-echo "Building prod version..."
-cp "config.prod.json" "config.json" || cp "/home/jenkins/frogtown/config/config.prod.json" "config.json";
-docker build -t gcr.io/frogtown/frogtown2020/prod:$1 .
-docker push gcr.io/frogtown/frogtown2020/prod:$1
+  echo "Building prod version..."
+  cp "config.prod.json" "config.json" || cp "/home/jenkins/frogtown/config/config.prod.json" "config.json";
+  docker build -t gcr.io/frogtown/frogtown2020/prod:$1 .
+  docker push gcr.io/frogtown/frogtown2020/prod:$1
+fi
 
 echo "Building local version..."
 cp "config.local.json" "config.json" || cp "/home/jenkins/frogtown/config/config.local.json" "config.json";
