fed3e28ecaa29cc00e04c8fe73fe5c5bbe7544c3
Frogtown Matt <batesmatthewj@gmail.com>
2021-07-01 18:32:35 -0500

    Change  to

--- src/server/scryfall_manager.test.ts
+++ src/server/scryfall_manager.test.ts
@@ -5,7 +5,7 @@ const scryfallManager = new ScryfallManager();
 test("Succeeds simple request.", (done) => {
   jest.setTimeout(30000);
   scryfallManager
-    .request<object>("https://api.scryfall.com/bulk-data")
+    .request<unknown>("https://api.scryfall.com/bulk-data")
     .then((result) => {
       expect(result).not.toBe(null);
       done();
