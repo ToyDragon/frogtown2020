6323b7443e1f7149741adbfbb143c78ffa24e97e
Thomas Kismarton <thomaskismarton@gmail.com>
2021-07-28 23:04:47 +0000

    Additional formatting for altPane, display is functional with dummy cards

--- src/views/deckviewer/alternate_art_popup_pane.ts
+++ src/views/deckviewer/alternate_art_popup_pane.ts
@@ -1,69 +1,3 @@
-/*import { MiscOptions } from "../shared/client/cardfilters/filter_misc_options";
-import { CardSearchBehavior } from "../shared/client/cardsearch_behavior";
-import { DataLoader } from "../shared/client/data_loader";
-import {
-  BaseCardRenderer,
-  CardRendererOptions,
-} from "../shared/client/renderers/base_card_renderer";
-import { CardRendererCompactGrid } from "../shared/client/renderers/card_renderer_compact_grid";
-import { CardRendererDetails } from "../shared/client/renderers/card_renderer_details";
-import { CardRendererGrid } from "../shared/client/renderers/card_renderer_grid";
-import { CardRendererList } from "../shared/client/renderers/card_renderer_list";
-import { CardRendererText } from "../shared/client/renderers/card_renderer_text";
-import { CardRenderArea } from "../shared/client/renderers/card_render_area";
-import ViewBehavior from "../shared/client/view_behavior";
-import { DeckViewerIncludedData } from "./types";
-
-export class altArtPane extends ViewBehavior<DeckViewerIncludedData> {
-  private altPane: HTMLElement = document.querySelector(
-    "altPane"
-  ) as HTMLElement;
-  private cardSearchUtil = new CardSearchBehavior(
-    this.dl,
-    (cardIds: string[], _miscOptions: MiscOptions) => {
-      this.altPaneRenderArea.UpdateCardList(cardIds);
-    }
-  );
-
-  private defaultAltPaneOptions: MiscOptions = {
-    "Show Duplicates": false,
-    "Stack Duplicates": false,
-    "Action Add": true,
-    "Action Remove": true,
-    "Action To Sideboard": true,
-    "Action Similar": true,
-    "Action Star": true,
-  };
-
-  private altPaneOptions: CardRendererOptions = {
-    dataLoader: this.dl,
-    cardArea: this.altPane,
-    scrollingParent: this.altPane,
-    allowEdit: false,
-    actionHandler: (action: string, cardId: string) => {
-      this.onSearchAction(action, cardId);
-    },
-  };
-
-  private baseRenderList: BaseCardRenderer[] = [
-    new CardRendererGrid(this.altPaneOptions),
-    new CardRendererList(this.altPaneOptions),
-    new CardRendererDetails(this.altPaneOptions),
-    new CardRendererCompactGrid(this.altPaneOptions),
-    new CardRendererText(this.altPaneOptions),
-  ];
-
-  private altPaneRenderArea = new CardRenderArea(
-    new DataLoader(null),
-    this.baseRenderList,
-    "altpanegroupers",
-    "altpanedisplay",
-    this.defaultAltPaneOptions,
-    null,
-    "AltPaneCards"
-  );
-}*/
-
 export class AlternateArtPane {
   constructor() {
     document.getElementById("exitIcon")?.addEventListener("click", function () {
@@ -73,11 +7,12 @@ export class AlternateArtPane {
 
   open(cardID: string): void {
     document.getElementById("altPane")!.style.visibility = "visible";
+    document.getElementById("cardContainer")!.style.background = "red";
     this.renderAltArts(cardID);
   }
 
   applyFilters(): void {
-    console.log("Apply filters");
+    console.log("Applying filters");
     return;
   }
 
